36e5f4d376bb71a0b1ec30d8602adc26d545af72
Phong Nguyen <84949014+Baegilpong@users.noreply.github.com>
2024-05-04 20:53:01 -0400

    fix naming error

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Login/Login.css";
 import Navbar from '../../components/Navbar/Navbar';
+
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
